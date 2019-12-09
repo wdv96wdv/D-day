913d02f48ba0089cd2640de5b908b23a9def0ef7
Soo-moon <tkeks96@gmail.com>
2019-12-09 21:12:50 +0900

    맵api 마커 , 저장시 마커위치 출력

--- app/src/main/java/halla/icsw/d_day/MainActivity.java
+++ app/src/main/java/halla/icsw/d_day/MainActivity.java
@@ -396,6 +396,7 @@ public boolean onOptionsItemSelected(MenuItem item) {
         try {
             List<Address> resultaddress = geocoder.getFromLocation(latitude,longitude,1);
             str = resultaddress.get(0).getCountryCode()+"   "+resultaddress.get(0).getAdminArea()+"   "+resultaddress.get(0).getThoroughfare();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
